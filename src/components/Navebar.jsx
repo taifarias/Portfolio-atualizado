e92cd1cda8828df331142f7c0291648d8af759f4
Tainá <141168586+taifarias@users.noreply.github.com>
2024-08-23 11:58:46 -0300

    c

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -5,7 +5,7 @@ import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 const Navbar = () => {
     return (
         <div className="linkSocial flex fixed w-full h-8 items-center justify-center space-x-10 bg-green-300 bg-opacity-45">
-        <a href="https://www.linkedin.com/in/taibarboza-devweb/" className="transition-transform duration-200 transform hover:scale-110">
+        <a href="https://github.com/taifarias" className="transition-transform duration-200 transform hover:scale-110">
             <IoLogoGithub size={25} />
         </a>
         <a href="https://www.linkedin.com/in/taibarboza-devweb/" className="transition-transform duration-200 transform hover:scale-110">
